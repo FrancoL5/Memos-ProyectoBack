bf2835ba926f0e773c47627e855de18aaf19adbb
FrancoL <francovalentin5005@gmail.com>
2022-10-10 23:00:32 -0300

    finish

--- src/controllers/messagesController.js
+++ src/controllers/messagesController.js
@@ -7,13 +7,12 @@ const createMessage = async (req, res) => {
 
         console.log(messages);
         await Promise.all(
-            messages.map(async (content) => {
-                const receiver_id = (await User.findOne({attributes:["user_id"], where: {user_name: content.receiver}})).user_id
-                console.log(content);
+            messages.receiver.map(async (receiverUser) => {
+                const receiver_id = (await User.findOne({attributes:["user_id"], where: {user_name: receiverUser}})).user_id
                 if(!receiver_id){
                     throw new Error("Uno de los usuarios no existe")
                 }
-                return await Message.create({ ...content,receiver_id })
+                return await Message.create({user_id:messages.user_id, user_name: messages.user_name ,content:messages.content, receiver: receiverUser,receiver_id })
             })
         )
         return res.status(200).json(true)
@@ -49,4 +48,18 @@ const getMessages = async (req, res) => {
     }
 }
 
-export { createMessage, getMessages }
+const deleteMessage = async (req, res) => {
+    try {
+        const {id} = req.body
+        const result = await Message.destroy({ restartIdentity: true, where: { id } })
+        if(!result){
+            throw new Error("Id incorrecta")
+        }
+        return res.status(200).json(!!result)
+    } catch (err) {
+        console.log(err)
+        return res.status(400).json("algo salio mal")
+    }
+}
+
+export { createMessage, getMessages, deleteMessage}
--- src/routes/message.routes.js
+++ src/routes/message.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createMessage, getMessages } from "../controllers/messagesController.js";
+import { createMessage, deleteMessage, getMessages } from "../controllers/messagesController.js";
 const routerMessage = express.Router();
 
 
 routerMessage.post("/messages", createMessage)
+routerMessage.post("/messages/delete", deleteMessage)
 routerMessage.get("/:id/messages/:flow", getMessages)
 
 export default routerMessage
